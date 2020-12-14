20dc4e4fad7edcc4e5c036418c0ae895bae881a2
Kumar <harsh4981@gmail.com>
2020-12-14 15:18:26 +0530

    CreateElement

---
+++ src/components/Hello/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = () => {
+    /*return (
+        <div>
+            
+        </div>
+    )*/
+    return React.createElement('div',null,React.createElement('h1',null,'Hello world'));
+}
+
+export default Hello
